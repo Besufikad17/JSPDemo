d79c0c8b75f7185817de8bd2acb92fbc2cb1f943
besufikad17 <besufikadtialhun1994@gmail.com>
2023-01-24 12:08:52 +0300

    file clean up

--- src/main/webapp/WEB-INF/classes/bean/LoginDao.java
+++
@@ -1,9 +0,0 @@
-package bean;
-import java.sql.*;
-public class LoginDao {
-
-    public static boolean validate(LoginBean bean){
-        System.out.println(bean.getUsername() + " " + bean.getPassword());
-        return bean.getUsername() == null || bean.getPassword() == null;
-    }
-}
